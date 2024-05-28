a86005df9d100441b5fa4b1b6197aa81903f9362
ArkhamTovar <100539695+ArkhamTovar@users.noreply.github.com>
2024-05-28 17:49:17 -0600

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROC spConsultaLibro
+(@P_Accion TINYINT,
+@P_Texto VARCHAR(15))
+AS
+BEGIN
+	SET NOCOUNT ON
+ 
+	IF @P_Accion = 1
+	BEGIN
+ 
+		SELECT IDLibro,
+			   CONCAT_WS(' ', Nombre,Genero,ISBN, Editorial, Precio) AS Libro
+			   --FORMAT(FecRegistro,'dd/MM/yyyy HH:mm') AS FecRegistro
+			   --iif(IsActivo= 1, 'Activo', 'InActico') AS Estatus
+		FROM LIBRO
+ 
+	END
+	--ELSE IF @P_Accion = 2
+--	BEGIN
+ 
+--		SELECT Matricula,
+--			   CONCAT_WS(' ', Nombre,APaterno,AMaterno,Grupo) AS Alumno
+--			   --FORMAT(FecRegistro,'dd/MM/yyyy HH:mm') AS FecRegistro
+--			   --iif(IsActivo= 1, 'Activo', 'InActico') AS Estatus
+--		FROM ALUMNO
+--		WHERE CONCAT(Nombre,APaterno,AMaterno) LIKE '%' + @P_Texto + '%'
+ 
+--	END
+ 
+--	SET NOCOUNT OFF
+END
---
+++ SQLQuery5.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROC sp_ActualizarLibro
+(@P_IDLibro INT,
+@P_Nombre VARCHAR(60),
+@P_Genero VARCHAR(60),  
+@P_ISBN INT, 
+@P_Editorial VARCHAR (60),
+@P_Precio INT)
+AS
+BEGIN
+	SET NOCOUNT ON
+ 
+	UPDATE CLIENTE SET Nombre = @P_Nombre,
+					   Genero = @P_Genero,
+					   ISBN = @P_ISBN,
+					   Editorial = @P_Editorial,
+					   Precio=@P_Precio
+					   --IsActivo = @P_IsActivo
+	WHERE IDLibro = @P_IDLibro
+ 
+ 
+	SET NOCOUNT OFF
+END
