e5621539e131eeba70351399d1c1cd3b384c85d8
ArkhamTovar <100539695+ArkhamTovar@users.noreply.github.com>
2024-05-28 17:19:46 -0600

    Add files via upload

---
+++ ENTITY/DTO/DTOconsultLibro.cs
@@ -0,0 +1,12 @@
+﻿namespace ENTITY.DTO
+{
+    public record DTOconsultLibro
+    {
+        public int? IDLibro { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Genero { get; set; }= string.Empty;
+        public string ISBN { get; set; }
+        public string Editorial { get; set; }= string.Empty;
+        public int Precio { get; set; }
+    }
+}
---
+++ ENTITY/Libro.cs
@@ -0,0 +1,13 @@
+﻿namespace ENTITY
+{
+    public record Libro
+    {
+        public int? IDLibro { get; set; }
+        public string Nombre { get; set; }
+        public string Genero { get; set; }
+        public string ISBN { get; set; }
+        public string Editorial { get; set; }
+        public int Precio { get; set; }
+
+    }
+}
